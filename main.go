dff062a3c43fafb63b6c49eace0671d066a03533
Brix <brixterporras@gmail.com>
2024-06-29 11:31:03 +0800

    feat: added csv write

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io"
 	"os"
+	"strconv"
 )
 
 type MoneyType struct {
@@ -40,8 +42,30 @@ func main() {
 
 	json.Unmarshal(byteValue, &data)
 
+	csvFile, err := os.Create("data.csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer csvFile.Close()
+
+	writer := csv.NewWriter(csvFile)
+	defer writer.Flush()
+
+	header := []string{"Date", "Total"}
+	if err := writer.Write(header); err != nil {
+		fmt.Println(err)
+	}
+
 	for i := 0; i < len(data.SalesAndTrafficByDate); i++ {
 		fmt.Printf("%+v\n", data.SalesAndTrafficByDate[i])
+
+		record := data.SalesAndTrafficByDate[i]
+		row := []string{record.Date, strconv.Itoa(record.SalesByDate.UnitsOrdered)}
+		if err := writer.Write(row); err != nil {
+			fmt.Println(err)
+		}
 	}
+
+	fmt.Println("CSV file created successfully")
 
 }
